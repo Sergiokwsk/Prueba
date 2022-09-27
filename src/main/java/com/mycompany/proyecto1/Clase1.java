1c9dcb0ecacd3fdbd713288ea579cc046ef63304
Sergiokwsk <sergio.kossowski@tiernoparla.es>
2022-09-27 20:09:56 +0200

    aded src

---
+++ src/main/java/com/mycompany/proyecto1/Clase1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author vespertino
+ */
+public class Clase1 {
+    public static void main(String[] args) {
+        //Printer
+        for(int i = 2;i < 11; i=i+2){
+            System.out.println(i);
+        
+        }//for
+        
+        
+        
+        
+    }
+    
+}
